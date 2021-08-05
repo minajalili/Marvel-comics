28141e6643c5ab66bef7d9b0f1d826dba6297586
minajalili <minajalili1997@gmail.com>
2021-08-05 18:37:31 +0430

    made new files for each component

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,13 +1,13 @@
 import React, {Fragment} from 'react';
 import { Field , ErrorMessage} from 'formik'
-import TextError from './TextError';
-//import './Login.css'
+import TextError from '../TextError/TextError';
+import './Input.css'
 
 function Input(props) {
     const { label, name, ...rest} = props
     return (
         <Fragment>
-        <div className="login__from--input">
+        <div className="inputContainer">
             <label htmlFor={name}>{label}</label>
             <Field id={name} name={name} placeholder={label} {...rest} />
         </div>
